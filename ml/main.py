e3fc8d67b3d7d27b1d51b64bc96ec58b0392fe0d
Aditya Vishwanath <adityav2004@gmail.com>
2016-09-10 17:04:14 -0400

    machine learning legit works omggggg

--- ml/main.py
+++ ml/main.py
@@ -11,10 +11,10 @@ import numpy as np
 BASE_URL = "https://data.phila.gov/resource/sspu-uyfa.json?$$app_token=bF12rIkELtbJ8PXnuzuZTWmVF"
 
 #REPLACE THESE WITH ACTUAL VALUES
-longitude = "%20-75.176386"
-latitude = "%2039.916687"
+longitude = "-75.176386"
+latitude = "39.916687"
 
-radius = "%20500" #set radius to 500 meters
+radius = "500" #set radius to 500 meters
 
 #Find all points around a specified radius
 location_url = BASE_URL+ ("&$where=within_circle(shape,%s,%s,%s)" % (latitude, longitude, radius))
@@ -39,15 +39,47 @@ u'dc_dist': u'01', u'dispatch_time': u'18:17:00'}
 training_data = []
 training_labels = []
 
+num_100 = 0
+num_200 = 0
+num_300 = 0
+num_400 = 0
+num_500 = 0
+num_600 = 0
+num_other = 0
+
 for d in data:
 	ucr = d.get('ucr_general')
+	ucr = int(ucr)
+	#Round down the UCR Number to the nearest 100
+	ucr = ucr - (ucr % 100)
 	if ucr != None:
 		if not (ucr == 700 or ucr == 1000 or ucr == 1100 or ucr == 1200 or ucr == 1300 or ucr == 1500 or (ucr > 1500 and ucr <= 2400) or ucr == 2600):
+			if ucr == 100:
+				num_100 += 1
+			elif ucr == 200:
+				num_200 += 1
+			elif ucr == 300:
+				num_300 += 1
+			elif ucr == 400:
+				num_400 += 1
+			elif ucr == 500:
+				num_500 += 1
+			elif ucr == 600:
+				num_600 += 1
+			else:
+				num_other += 1
 			training_data.append((int(d.get('hour'))))
-			ucr_general_number = int(d.get('ucr_general'))
-			#Round down the UCR Number to the nearest 100
-			rounded_ucr_general = ucr_general_number - (ucr_general_number % 100)
-			training_labels.append(rounded_ucr_general)
+			training_labels.append(ucr)
+
+print training_labels
+
+print "Number of homicides = " + str(num_100)
+print "Number of rapes = " + str(num_200)
+print "Number of robberies = " + str(num_300)
+print "Number of assaults = " + str(num_400)
+print "Number of burglaries = " + str(num_500)
+print "Number of thefts = " + str(num_600)
+print "Number of other crimes = " + str(num_other)
 
 #Need to reshape the training data because the vector is in 1D
 all_training_data = np.array(training_data).reshape(-1,1)
@@ -73,10 +105,17 @@ time0 = time()
 classifier.fit(features_train, labels_train)
 
 #Get second timestamp
-training_time = round(time()-time0, 4)
+training_time = round(time() - time0, 4)
 
 print "The training time was = " + str(training_time) + " seconds"
 
+'''
 #Use the testing data to find the accuracy of the model
 accuracy = classifier.score(features_test, labels_test)
 print "The Accuracy Score is = " + str(round(accuracy * 100, 3)) + " %"
+'''
+
+#Test for a random time of the day
+prediction = classifier.predict(np.array([18]).reshape(-1,1))
+print "The prediction is = " + str(prediction)
+
